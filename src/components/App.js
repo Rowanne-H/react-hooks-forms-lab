ba3d385ec9063322dc8d7dbcf48587dc17969332
YUERONG HUANG <rowanne.h@hotmail.com>
2024-04-03 22:30:24 +1100

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,20 @@ import itemData from "../data/items";
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  console.log(itemData)
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function onItemFormSubmit(item) {
+    setItems([...items, item])
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={onItemFormSubmit}/>
     </div>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,33 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
+
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  const [searchItem, setSearchItem] = useState("All");
 
-    return item.category === selectedCategory;
+  function handleSearchChange(event) {
+    setSearchItem(event.target.value); 
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (searchItem === "All" && selectedCategory === "All") {return true}
+    else if (searchItem !== "All" && selectedCategory === "All") {return item.name.includes(searchItem)}
+    else if (searchItem === "All" && selectedCategory !== "All") {return item.category === selectedCategory}
+    else {return item.category === selectedCategory && item.name.includes(searchItem)}
   });
+
+ 
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter search={searchItem} onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
